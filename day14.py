74dfa5167ac5b858839580160e6ca175f3f454b2
Piotr Kozka <piotoor@gmail.com>
2023-12-20 20:14:29 +0100

    Day14b solution

--- day14.py
+++ day14.py
@@ -27,6 +27,24 @@ def tilt_north(data):
     return data
 
 
+def tilt_west(data):
+    for i in range(0, len(data)):
+        free_col = 0
+        for j in range(len(data[i])):
+            if data[i][j] == '.':
+                if j == 0 or data[i][j - 1] != '.':
+                    free_col = j
+            elif data[i][j] == 'O':
+                if j > 0 and data[i][j - 1] == '.':
+                    data[i][free_col] = 'O'
+                    data[i][j] = '.'
+                    free_col += 1
+            else:
+                free_col = i + 1
+
+    return data
+
+
 def tilt_south(data):
     for j in range(len(data[0])):
         free_row = len(data) - 1
@@ -35,7 +53,7 @@ def tilt_south(data):
                 if i == len(data) - 1 or data[i + 1][j] != '.':
                     free_row = i
             elif data[i][j] == 'O':
-                if i + 1 < len(data) - 1 and data[i + 1][j] == '.':
+                if i + 1 < len(data) and data[i + 1][j] == '.':
                     data[free_row][j] = 'O'
                     data[i][j] = '.'
                     free_row -= 1
@@ -45,20 +63,20 @@ def tilt_south(data):
     return data
 
 
-def tilt_north(data):
-    for j in range(len(data[0])):
-        free_row = 0
-        for i in range(0, len(data)):
+def tilt_east(data):
+    for i in range(len(data)):
+        free_col = len(data[i]) - 1
+        for j in range(len(data[i]) - 1, -1, -1):
             if data[i][j] == '.':
-                if i == 0 or data[i - 1][j] != '.':
-                    free_row = i
+                if j == len(data[i]) - 1 or data[i][j + 1] != '.':
+                    free_col = j
             elif data[i][j] == 'O':
-                if i > 0 and data[i - 1][j] == '.':
-                    data[free_row][j] = 'O'
+                if j + 1 < len(data[i]) and data[i][j + 1] == '.':
+                    data[i][free_col] = 'O'
                     data[i][j] = '.'
-                    free_row += 1
+                    free_col -= 1
             else:
-                free_row = i + 1
+                free_col = i - 1
 
     return data
 
@@ -82,7 +100,12 @@ def total_load(data):
     return ans
 
 
+def hash_board(data):
+    ans = []
+    for x in data:
+        ans.append(tuple(x))
 
+    return tuple(ans)
 
 
 def total_load_cycles(data):
@@ -90,15 +113,80 @@ def total_load_cycles(data):
     for x in data:
         print(x)
 
-    data = tilt_south(data)
+    # for i in range(3):
+    #     data = tilt_north(data)
+    #     print()
+    #     for x in data:
+    #         print(x)
+    #     data = tilt_west(data)
+    #     print()
+    #     for x in data:
+    #         print(x)
+    #     data = tilt_south(data)
+    #     print()
+    #     for x in data:
+    #         print(x)
+    #     data = tilt_east(data)
+    #     print()
+    #     for x in data:
+    #         print(x)
+    #     print("-----------")
+    # return 3
+    h = {}
+    num_of_iters = 1000000000
+    s = -1
+    b = -1
+    for i in range(1, num_of_iters + 1):
+        if i % 10000 == 0:
+            print(i)
+            # for x in hashed_data:
+            #     print(x)
+            # print()
 
-    print()
-    for x in data:
-        print(x)
+        data = tilt_north(data)
+        data = tilt_west(data)
+        data = tilt_south(data)
+        data = tilt_east(data)
+
+        hashed_data = hash_board(data)
+
+        if hashed_data in h:
+            if s == -1:
+                s = h[hashed_data]
+                b = i
+                print("repeated curr_iter = {} prev_iter = {}".format(i, h[hashed_data]))
+                print("values = {}".format(h.values()))
+                break
+
+        else:
+            h[hashed_data] = i
+        # if i == 3:
+        #     break
+
+    a = s - 1
+    b = b - s - 2
+    c = num_of_iters
+
+    ans_idx = s + c - a - ((c - a) // b) * b
+    print(a, b, c, s, ans_idx)
+
+    ans = 0
+    for key, val in h.items():
+        if val == ans_idx:
+
+            for x in key:
+                print(x)
+            for j in range(len(key[0])):
+                for i in range(0, len(key)):
+                    if key[i][j] == 'O':
+                        ans += len(key) - i
+
+    return ans
+
+
 def day14_a():
     data = parse_day14_a()
     print("day14a = {}".format(total_load(data)))
-
 
 
 def day14_b():
